25b6b9f9a01b415e10e1f77cbf44bc151badac11
tkc(mac) <takashi_alien@yahoo.co.jp>
2022-06-10 13:46:11 +0900

    APIエンドポイント名の定数を定義

--- lib/define/api_const.dart
+++ lib/define/api_const.dart
@@ -4,4 +4,9 @@ class ApiConst {
   static const registerUser = 'registerUser';
   static const createRoom = 'createRoom';
   static const joinRoom = 'joinRoom';
+  static const exitRoom = 'exitRoom';
+  static const startGame = 'startGame';
+  static const setTheme = 'setTheme';
+  static const setAnswer = 'setAnswer';
+  static const setPoints = 'setPoints';
 }
