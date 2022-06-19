064c35c6260ce652a59d6e2419e2cc8aae7b0173
tkc(mac) <takashi_alien@yahoo.co.jp>
2022-06-19 22:02:22 +0900

    GetRoomStreamInteractor内でアクティブ状態を通知する機構を実装

--- lib/domain/interactor/get_room_stream_interactor.dart
+++ lib/domain/interactor/get_room_stream_interactor.dart
@@ -6,10 +6,22 @@ import 'package:fripo/domain/use_case/get_room_stream_use_case.dart';
 class GetRoomStreamInteractor implements GetRoomStreamUseCase {
   @override
   Stream<RoomInfo> call() {
-    return FirebaseDatabase.instance
-        .ref('rooms/${AppData.roomId}')
-        .onValue
-        .map((event) {
+    final database = FirebaseDatabase.instance;
+    final roomRef = database.ref('rooms/${AppData.roomId}');
+    final activeRef = roomRef.child('members/${AppData.userId}/isActive');
+
+    database.ref('.info/connected').onValue.listen((event) {
+      print('connected listen: ${event.snapshot.value}');
+      if (event.snapshot.value == false) {
+        return;
+      }
+      activeRef.onDisconnect().set(false).then((_) {
+        print('connection false, completed');
+        activeRef.set(true);
+      });
+    });
+
+    return roomRef.onValue.map((event) {
       print(event.snapshot.value);
       return RoomInfo.fromMap(event.snapshot.value as Map<dynamic, dynamic>);
     });
