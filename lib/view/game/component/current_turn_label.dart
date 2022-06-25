5b00403a548f273463a04d52e6da8295638267ba
tkc(mac) <takashi_alien@yahoo.co.jp>
2022-06-25 17:32:11 +0900

    メンバー一覧コンポーネントの仮実装をマージしようとして色々ミスったのを無理やり修正

---
+++ lib/view/game/component/current_turn_label.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fripo/view_model/game_view_model.dart';
+
+class CurrentTurnLabel extends StatelessWidget {
+  const CurrentTurnLabel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text('TURN'),
+        Text(
+          GameViewModel.select(
+            context,
+            (vm) => vm.roomInfo?.currentTurn?.toString() ?? '?',
+          ),
+          style: const TextStyle(
+            fontSize: 32,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/view/game/component/member_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:fripo/view/app_common/profile_icon_with_life.dart';
+import 'package:fripo/view_model/game_view_model.dart';
+
+class MemberList extends StatelessWidget {
+  const MemberList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final maxLife =
+        GameViewModel.select(context, (vm) => vm.roomInfo?.defaultLife);
+    final members = GameViewModel.select(context, (vm) => vm.roomInfo?.members);
+
+    if (maxLife == null ||
+        members == null ||
+        members.values.any((member) => member.life == null)) {
+      return const SizedBox();
+    }
+
+    return SizedBox(
+      height: 56,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: members.values.map((member) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            child: FittedBox(
+              child: ProfileIconWithLife(
+                url: member.iconUrl,
+                maxLife: maxLife,
+                life: member.life!,
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
