7f332bdb9935709d6bb40153365d2531fba4b711
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2022-07-04 16:54:26 +0900

    利用規約とプライバシーポリシーの表示を実装

--- lib/view/privacy_policy/privacy_policy_screen.dart
+++ lib/view/privacy_policy/privacy_policy_screen.dart
@@ -1,13 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class PrivacyPolicyScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PrivacyPolicyScreen extends StatefulWidget {
   const PrivacyPolicyScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  @override
+  void initState() {
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("PrivacyPolicy"),
+        title: const Text("プライバシーポリシー"),
+      ),
+      body: const WebView(
+        initialUrl:
+            'https://fripo-app.notion.site/6c7bac49197b4b0e80a4217ecb676928',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }
--- lib/view/terms/terms_screen.dart
+++ lib/view/terms/terms_screen.dart
@@ -1,13 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class TermsScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class TermsScreen extends StatefulWidget {
   const TermsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TermsScreen> createState() => _TermsScreenState();
+}
+
+class _TermsScreenState extends State<TermsScreen> {
+  @override
+  void initState() {
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Terms"),
+        title: const Text("利用規約"),
+      ),
+      body: const WebView(
+        initialUrl:
+            'https://fripo-app.notion.site/efa7ac57a70a487a8a6dc8896719ad9a',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }
